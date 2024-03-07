2b8816b8317b107c80c4d4185f9fea5dd038a953
MostakemHossain <mostakememon123@gmail.com>
2024-03-07 11:56:19 +0600

    post new class data api create

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const cors= require('cors');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000
 
  
+//middleware
+app.use(cors());
+app.use(express.json());
+
 
 // mongodb connextion
 
@@ -23,12 +28,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // create a dababase and connection
+    const database= client.db("yoga-master");
+    const userCollection= database.collection("users");
+    const classesCollection= database.collection("classes");
+    const cartCollection= database.collection("cart");
+    const paymentCollection= database.collection("payments");
+    const enrolledCollection= database.collection("enrolled");
+    const appliedCollection= database.collection("applied");
+
+
+    // classes routes here
+    app.post('/new-class',async (req,res)=>{
+        const newClass= req.body;
+        const result= await classesCollection.insertOne(newClass);
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
