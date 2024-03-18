e838d976dd4899095574f04a3db8e6937f4f6c79
MostakemHossain <mostakememon123@gmail.com>
2024-03-18 16:45:53 +0600

    Popular class section added

---
+++ frontend/src/pages/Home/PopularClasses/Card.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+
+
+const Card = ({ item }) => {
+    const { _id, name, image, availableSeats, price, totalEnrolled } = item
+    console.log(image)
+    return (
+        <div className="shadow-lg rounded-lg p-3 flex flex-col justify-between border border-secondary overflow-hidden m-4">
+            <img className="w-full h-[350px]" src={image} alt="" />
+            <div className="p-4">
+                <h1 className="text-xl font-semibold mb-2 dark:text-white">{name}</h1>
+                <p className="text-gray-600 mb-2">Available seats : {availableSeats}</p>
+                <p className="text-red-600 font-bold mb-2">Price : ${price}</p>
+                <p className="text-secondary font-bold mb-2">Total Students : {totalEnrolled}</p>
+
+                <Link to={`/class/${_id}`} className="text-center mt-2">
+                    <button className="px-2 w-full py-2 rounded-xl bg-secondary text-white font-bold mt-2">view course</button>
+                </Link>
+
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- frontend/src/pages/Home/PopularClasses/PopularClasses.jsx
+++ frontend/src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import useAxiosFetch from "../../../hooks/useAxiosFetch";
+import Card from "./Card";
 
 
 const PopularClasses = () => {
@@ -12,13 +13,20 @@ const PopularClasses = () => {
         }
         fetchClasses();
     }, [])
+
     return (
         <div className="md:w-[80%] mx-auto my-36">
-            <div>
+            <div className="dark:text-white">
                 <h1 className="text-5xl font-bold text-center uppercase">Our <span className="text-secondary">Popular</span> Classes</h1>
                 <div className="w-[40%] text-center mx-auto my-4">
                     <p className="text-gray-500">Explore our popular classes.Here is some popular classes based on home many students enrolled.</p>
                 </div>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+                {
+                    classes.map((item, index) => <Card key={index} item={item} />)
+                }
             </div>
         </div>
     )
